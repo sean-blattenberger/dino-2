a60bf9366c9941871d539776960231fdaac3e680
sean-blattenberger <sean.blattenberger@gmail.com>
2018-06-09 15:37:02 -0600

    separated header and footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,10 @@
 import React, { Component } from "react";
 import "../App.css";
-import Profiles from "../components/Profiles"
-// import dinoLogo from "../assets/g-dino.png";
+import Profiles from "../components/Profiles";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
-const Header = () => {
-  return (
-  <header>
-    <img src="./assets/g-dino.png" alt="Dinosaur Logo" />
-  </header>
-  )
-};
-const Footer = () => {
-  return (
-    <footer>
-      <small>Copyright 2018</small>
-    </footer>
-  );
-};
+
 
 class App extends Component {
   state = {
